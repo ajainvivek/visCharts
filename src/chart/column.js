841117aebf04dbc2387d590b86e464c0197c6746
Ajain Vivek <ajain.vivek@sociomobileplus.com>
2015-02-02 00:47:50 +0530

    Folder Structure

---
+++ src/chart/column.js
@@ -0,0 +1,14 @@
+/**
+ * Interactive Visualisation Charts (c) 2015 Ajain Vivek
+ *
+ * Licensed under MIT
+ */
+
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
+/*global define, $ */
+
+var Column = function () {
+    "use strict";
+    //TODO
+};
+
---
+++ src/chart/line.js
@@ -0,0 +1,13 @@
+/**
+ * Interactive Visualisation Charts (c) 2015 Ajain Vivek
+ *
+ * Licensed under MIT
+ */
+
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
+/*global define, $ */
+
+var Line = function () {
+    "use strict";
+    //TODO
+};
